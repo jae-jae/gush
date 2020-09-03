3c71c41ac8f8ae48c1bb865516aa5093db9cf590
Jaeger <JaegerCode@gmail.com>
2020-09-03 22:38:19 +0800

    feat: show version

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"gush/util"
 )
@@ -9,21 +10,19 @@ const VERSION = "1.0"
 
 var showVersion = new(bool)
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gush",
 	Short: "Project deployment tool",
 	Long:  `Project deployment tool.`,
-	//Run: func(cmd *cobra.Command, args []string) {
-	//	if *showVersion {
-	//		fmt.Println("gush version " + VERSION)
-	//	}
-	//},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		util.Fatalln(err.Error())
+	}
+
+	if *showVersion {
+		fmt.Println("gush version " + VERSION)
 	}
 }
 
